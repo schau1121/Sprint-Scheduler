1d2059bd35d2923d872cc9ec63a0ef67c53a1cc8
schau1121 <simon.chau1121@gmail.com>
2021-05-06 16:54:40 -0700

    Added sortstrategy.hpp file

---
+++ header/sortstrategy.hpp
@@ -0,0 +1,13 @@
+#ifndef __SORTSTRATEGY_HPP__
+#define __SORTSTRATEGY_HPP__
+
+#include "base.hpp"
+
+class SortStrategy {
+	public:
+		//This should return a new priority queue that is sorted
+		//We need to deallocate the memory from the previous priority queue after sorting
+		virtual Base* sort(Base* priorityQueue, int numSubTasks) = 0;
+};
+
+#endif
