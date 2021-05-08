827593a74faa5207c78a0bf700cc5e331773f9d7
Simon Chau <simon.chau1121@gmail.com>
2021-05-07 23:09:24 -0700

    Fixed compilation errors from inheritance

--- header/task.hpp
+++ header/task.hpp
@@ -5,7 +5,13 @@
 
 class Task : public Base {
 public:
-    Task(string name, string dueDate, string details, int priority);
+    Task(string name, string dueDate, string details, int priority) {
+        this->name = name;
+        this->dueDate = dueDate;
+        this->details = details;
+        this->priority = priority;
+        this->completed = false;
+    };
     ~Task();
     virtual void display() const;
     virtual void edit();
@@ -17,19 +23,48 @@ protected:
     string dueDate;
 };
 
-class TaskMock1 : public Task {
+class TaskMock1 : public Base {
 public:
-    TaskMock1() : Task("Task 1", "some date", "some details", 0) {};
+    TaskMock1() {
+        this->name = "Task 1";
+        this->dueDate = "some date";
+        this->details = "some details";
+        this->priority = 2;
+        this->completed = false; 
+    };
     virtual void display() const {
         cout << name << endl;
         cout << dueDate << endl;
         cout << details << endl;
-    }
+    };
+    virtual void edit() {};
+    virtual void del() {};
+private:
+    string dueDate;
+    bool completed;
+    int priority;
 };
 
-class TaskMock2 : public Task {
+class TaskMock2 : public Base {
 public:
-    TaskMock2() : Task("Task 2", "another date", "different details", 3) {};
+    TaskMock2() {
+        this->name = "Task 2";
+        this->dueDate = "a different date";
+        this->details = "more details";
+        this->priority = 4;
+        this->completed = false; 
+    };
+    virtual void display() const {
+        cout << name << endl;
+        cout << dueDate << endl;
+        cout << details << endl;
+    };
+    virtual void edit() {};
+    virtual void del() {};
+private:
+    string dueDate;
+    bool completed;
+    int priority;
 };
 
 #endif
--- header/tasklist.hpp
+++ header/tasklist.hpp
@@ -14,22 +14,36 @@ protected:
     string dueDate = ""; 
 public:
     TaskList(string name, string details, int priority);
-    virtual void display() const;
-    virtual void del();
-    virtual void edit();
+    virtual void display() const {};
+    virtual void del() {};
+    virtual void edit() {};
     void addSubTask(Base* task);
-    bool isCompleted();
-    vector<Base*> getQueue() const { return priorityQueue; }
+    bool isCompleted() { return true; };
+    vector<Base*> getQueue() const { return priorityQueue; };
 };
 
 //this task list is initialized with two task mocks
-class TaskListMock : public TaskList {
+class TaskListMock : public Base {
+protected:
+    int priority;
+    bool completed;
+    vector<Base*> priorityQueue;
+    int numSubTasks;
+    //set to empty string to differentiate between tasks and task lists when sorting by date
+    string dueDate = ""; 
 public:
-    TaskListMock() : TaskList("TaskList 1", "some details", 0) {
+    TaskListMock() {
+        name = "Homework";
+        details = "";
+        completed = false;
+        priority = 4;
         numSubTasks = 2;
         priorityQueue.push_back(new TaskMock1());
         priorityQueue.push_back(new TaskMock2());
     }
+    virtual void display() const {};
+    virtual void del() {};
+    virtual void edit() {};
 };
 
 #endif
