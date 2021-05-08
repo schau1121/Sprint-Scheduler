1d793f30835071e4a36dc30b76c29291f0ef328c
Simon Chau <simon.chau1121@gmail.com>
2021-05-07 22:19:09 -0700

    Implemented constructor and addSubTask function

---
+++ src/tasklist.cpp
@@ -0,0 +1,19 @@
+#ifndef __TASKLIST_CPP__
+#define __TASKLIST_CPP__
+
+#include "tasklist.hpp"
+
+TaskList::TaskList(string name, string details, int priority) {
+    this->name = name;
+    this->details = details;
+    this->priority = priority;
+    this->completed = false;
+    this->numSubTasks = 0;
+}
+
+TaskList::addSubTask(Base* task) {
+    priorityQueue.push_back(task);
+    numSubTasks++;
+}
+
+#endif
