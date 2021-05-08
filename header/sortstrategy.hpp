77d8bfd1b76ec1f8cd4a58392eb9a6877dd4cd0e
Simon Chau <simon.chau1121@gmail.com>
2021-05-07 22:26:51 -0700

    Updated sort strategy headers with vector return type

--- header/sortstrategy.hpp
+++ header/sortstrategy.hpp
@@ -7,7 +7,7 @@ class SortStrategy {
 public:
 	//This should return a new priority queue that is sorted
 	//We need to deallocate the memory from the previous priority queue after sorting
-	virtual Base* sort(Base* priorityQueue, int numSubTasks) = 0;
+	virtual vector<Base*> sort(vector<Base*> priorityQueue, int numSubTasks) = 0;
 };
 
 #endif
