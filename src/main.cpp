84bf69e509990fc490be398f26c74c1c58ecca95
schau1121 <simon.chau1121@gmail.com>
2021-05-13 11:09:13 -0700

    Added global vectors for all objects and implemented menu function for adding a task to a list

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <vector>
+#include "../header/base.hpp"
+#include "../header/sortstrategy.hpp"
+#include "../header/task.hpp"
+#include "../header/event.hpp"
+#include "tasklist.cpp"
 
 using namespace std;
 
@@ -12,6 +18,30 @@ using namespace std;
     So when our app initializes we should parse through the json and collect all the objects
     so that we can edit & display them
     */
+
+vector<Base*> allObjects;
+vector<Task*> allTasks;
+vector<TaskList*> allLists;
+vector<Event*> allEvents;
+
+void printAllTasks() {
+    for(int i = 0; i < allTasks.size(); i++) {
+        cout << i+1 << ". ";
+        allTasks[i]->display();
+    }
+}
+
+void addTaskToTaskList(TaskList* currList) {
+    int userInput;
+    printAllTasks();
+    cout << "Select a task to add to this list by entering its number: ";
+    cin >> userInput;
+    Task* currTask = allTasks[userInput - 1];
+    currList->addSubTask(currTask);
+    cout << "Added task #" << userInput << " to the list." << endl;
+    //currTask->setAssigned(true); setAssigned needs to be created
+    return;
+}
 
 int main() {
 
