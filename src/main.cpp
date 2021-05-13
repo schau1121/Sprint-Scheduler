0d7b5f04c983debf070edd7942994de139d0a76e
schau1121 <simon.chau1121@gmail.com>
2021-05-13 11:37:30 -0700

    Added task constructor, and setters in task.cpp and added createTask function in main

--- src/main.cpp
+++ src/main.cpp
@@ -20,14 +20,16 @@ using namespace std;
     */
 
 vector<Base*> allObjects;
-vector<Task*> allTasks;
-vector<TaskList*> allLists;
-vector<Event*> allEvents;
+vector<Base*> allTasks;
+vector<Base*> allLists;
+vector<Base*> allEvents;
 
 void printAllTasks() {
     for(int i = 0; i < allTasks.size(); i++) {
-        cout << i+1 << ". ";
-        allTasks[i]->display();
+        //if(!allTasks[i]->isAssigned()) { isAssigned neets to be created
+            cout << i+1 << ". ";
+            allTasks[i]->display();
+        //}
     }
 }
 
@@ -36,11 +38,36 @@ void addTaskToTaskList(TaskList* currList) {
     printAllTasks();
     cout << "Select a task to add to this list by entering its number: ";
     cin >> userInput;
-    Task* currTask = allTasks[userInput - 1];
+    Base* currTask = allTasks[userInput - 1];
     currList->addSubTask(currTask);
     cout << "Added task #" << userInput << " to the list." << endl;
     //currTask->setAssigned(true); setAssigned needs to be created
     return;
+}
+
+//User can only store one line of details as of right now
+void createTask() {
+    string name;
+    string date = "";
+    string details = "";
+    int priority = 0;
+    cout << "Creating a task..." << endl;
+    cout << "Enter a name: ";
+    cin >> name;
+    if(name == "") {
+        cout << "ERROR: NEED TASK NAME" << endl;
+        return;
+    }
+    cout << "\nEnter a due date as MM/DD/YY: ";
+    cin >> date;
+    cout << "\nEnter details: ";
+    getline(cin, details);
+    cout << "\nEnter priority as 0-5: ";
+    cin >> priority;
+    Base* newTask = new Task(name, date, details, priority);
+    allObjects.push_back(newTask);
+    allTasks.push_back(newTask);
+    cout << "\nCreated!" << endl;
 }
 
 int main() {
---
+++ src/task.cpp
@@ -0,0 +1,29 @@
+#ifndef __TASK_CPP__
+#define __TASK_CPP__
+
+#include "../header/task.hpp"
+
+Task::Task(string name, string dueDate = "", string details = "", int priority = 0) {
+    if(name == "") {
+        throw invalid_argument("Empty name passed into task constructor");
+    }
+    if(dueDate[2] != '/' || dueDate[5] != '/') {
+        throw invalid_argument("Wrong date format passed into task constructor");
+    }
+    this->name = name;
+    this->dueDate = dueDate;
+    this->details = details;
+    this->priority = priority;
+    this->completed = false;
+    this->assigned = false;
+}
+
+void Task::setCompleted(bool isComplete) {
+    completed = isComplete;
+}
+
+void Task::setAssigned(bool isAssigned) {
+    assigned = isAssigned;
+}
+
+#endif
