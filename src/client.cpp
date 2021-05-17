4cfbfcf13d627337b31abb9be6eb763cf58a44d3
AnhQNguyen <aqnguyen1206@gmail.com>
2021-05-17 12:10:49 -0700

    constructor and destructor

--- src/client.cpp
+++ src/client.cpp
@@ -6,7 +6,40 @@
 
 using namespace std;
 
+client::client() {
+	//call load function to populate the vector data members
+	
+	this->sortingMethod = "priority" //default sorting method
 
+}
+
+
+
+
+client::~client() {
+	for (auto i : allObjects) {
+		delete i;
+	}
+	allObjects.clear();
+
+	for(auto i : allTasks) {
+		delete i;
+	}
+	allTaks.clear();
+	
+	for(auto i : allLists) {
+		delete i;
+	}
+	allLists.clear();
+
+	for(auto i : allEvents) {
+		delete i;
+	}
+	allEvents.clear();
+
+	//delete all pointers within the vector data members
+	//clear the contents of the vectors
+}
 
 
 
