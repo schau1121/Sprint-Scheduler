d0df99f552d2e7ddc06c17e35e8f58e109a1ae2d
AnhQNguyen <66961614+AnhQNguyen@users.noreply.github.com>
2021-05-17 12:21:32 -0700

    Update client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -43,38 +43,7 @@ client::~client() {
 
 
 
-client::displayMenu() {
-	char userInput = ' ';
 
- 	do {
-		cout << "Select an option or press 'q' to quit" << endl;
-		cout << "1. View (press 'v')" << endl;
-		cout << "2. Create a task, tasklist, or an event (press 'c')" << endl;
-		
-		cin >> userInput;
-
-
-		switch(userInput) {
-			case 'V':
-			case 'v':
-				cout << "FIXME: Call view function" << endl;
-				break;
-			case 'C':
-			case 'c':
-				cout << "FIXME: Call create function" << endl;
-				break;
-			default:
-				cout << "ERROR: Invalid Input, please try again or press 'q' to quit" << endl;
-				break;
-		}
-	}
-	while(userInput != 'q' || userInput != 'Q');
-
-
-	//call save function before return
-	
-	return;
-}
 
 
 
