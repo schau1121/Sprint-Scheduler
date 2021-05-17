0551ed5f383818ab87a29408d302bc8fcb293641
AnhQNguyen <aqnguyen1206@gmail.com>
2021-05-17 11:59:26 -0700

    display menu function skeleton

---
+++ src/client.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "../header/client.hpp"
+
+using namespace std;
+
+
+//constructor
+client::client() {
+	this->sortingMethod = "priority";
+
+}
+
+
+client::displayMenu() {
+	char userInput = ' ';
+
+ 	do {
+		cout << "Select an option or press 'q' to quit" << endl;
+		cout << "1. View (press 'v')" << endl;
+		cout << "2. Create a task, tasklist, or an event (press 'c')" << endl;
+		
+		cin >> userInput;
+
+
+		switch(userInput) {
+			case 'V':
+			case 'v':
+				cout << "FIXME: Call view function" << endl;
+				break;
+			case 'C':
+			case 'c':
+				cout << "FIXME: Call create function" << endl;
+				break;
+			default:
+				cout << "ERROR: Invalid Input, please try again or press 'q' to quit" << endl;
+				break;
+		}
+	}
+	while(userInput != 'q' || userInput != 'Q');
+
+
+	//call save function before return
+	
+	return;
+}
+
+
+
