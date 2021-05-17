63ad18f5817f2c26b4dea3efdaedbbaf8c385b4a
schau1121 <simon.chau1121@gmail.com>
2021-05-17 16:46:38 -0700

    Added second unit test for SortByPriority

--- tests/sortpriority_test.cpp
+++ tests/sortpriority_test.cpp
@@ -19,5 +19,18 @@ TEST(sortByPriority_test, simple_test_tasks1) {
     }
 }
 
+TEST(sortByPriority_test, simple_test_tasks2) {
+    vector<Base*> queue;
+    SortByPriority sortingStrategy;
+    queue.push_back(new Task("CS100", "06/21/21", "", 1));
+    queue.push_back(new Task("MATH149B", "05/30/21", "", 5));
+    queue.push_back(new Task("MATH146C", "07/04/21", "", 3));
+    vector<string> expected = {"MATH149B", "MATH146C", "CS100"};
+    queue = sortingStrategy.sort(queue);
+    for(int i = 0; i < queue.size(); i++) {
+        EXPECT_EQ(queue[i]->getName(), expected[i]);
+    }
+}
+
 
 #endif
