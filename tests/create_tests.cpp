ca0c1d564c024d15dc05250d69a6f5249e799807
scottyallenp <spete020@ucr.edu>
2021-05-20 11:31:24 -0700

    adding all passing task constructor tests

--- tests/create_tests.cpp
+++ tests/create_tests.cpp
@@ -3,32 +3,60 @@
 #include"../src/client.cpp"
 #include "gtest/gtest.h"
 
+//					Event Tests
 
-TEST(Valid_Event_Constructor, valid_name) {
+TEST(Valid_Event_Constructor, Valid_Name) {
     Base* test = new Event("homework", "04/12/20", "april/19/12 PM", "finish homework", 5);
     EXPECT_EQ(test->getName(), "homework");
 }
 
-TEST(Valid_Event_Constructor, valid_due_date) {
+TEST(Valid_Event_Constructor, Valid_Due_Date) {
     Base* test = new Event("", "04/12/20", "april/19/12 PM", "finish homework", 5);
     EXPECT_EQ(test->getDate(), "04/12/20");
 }
 
-TEST(Valid_Event_Constructor, valid_time) {
+TEST(Valid_Event_Constructor, Valid_Time) {
     Base* test = new Event("", "04/12/20", "april/19/12PM", "finish homework", 5);
     EXPECT_EQ(test->getTime(), "april/19/12PM");
 }
 
-TEST(Valid_Event_Constructor, valid_details) {
+TEST(Valid_Event_Constructor, Valid_Details) {
     Base* test = new Event("", "04/12/20", "april/19/12 PM", "finish homework", 5);
     EXPECT_EQ(test->getDetails(), "finish homework");
 }
 
-TEST(Valid_Event_Constructor, valid_duration) {
+TEST(Valid_Event_Constructor, Valid_Duration) {
     Base* test = new Event("", "04/12/20", "april/19/12 PM", "finish homework", 5);
     EXPECT_EQ(test->getDuration(), 5);
 }
- 
+
+
+//					Task Tests 
+
+TEST(Valid_Task_Constructor, Valid_Name) {
+    Base* test = new Task("project", "11/11/21", "finish project", 3);
+    EXPECT_EQ(test->getName(), "project");
+} 
+
+TEST(Valid_Task_Constructor, Valid_Date) {
+    Base* test = new Task("project", "11/11/21", "finish project", 3);
+    EXPECT_EQ(test->getDate(), "11/11/21");
+}
+
+TEST(Valid_Task_Constructor, Valid_Details) {
+    Base* test = new Task("project", "11/11/21", "finish project", 3);
+    EXPECT_EQ(test->getDetails(), "finish project");
+}
+
+TEST(Valid_Task_Constructor, Valid_Priority) {
+    Base* test = new Task("project", "11/11/21", "finish project", 3);
+    EXPECT_EQ(test->getPriority(), 3);
+}
+
+
+TEST(Task_Constructor_test, null_name) {
+    EXPECT_THROW(Task("", "11/21/21", "Birthday coming up", 0), invalid_argument);
+}
 
 
 #endif
