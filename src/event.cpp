4b17e84a9bebe3fc3c93c65953bf594cd3218189
Simon Chau <simon.chau1121@gmail.com>
2021-05-20 15:43:31 -0700

    Update event.cpp

--- src/event.cpp
+++ src/event.cpp
@@ -2,6 +2,20 @@
 #define __EVENT_CPP__
 #include "../header/event.hpp"
 
+//Date format: MM/DD/YY
+//Time: HH:MM AM/PM
+/*if(date[2] != '/' || date[5] != '/') {
+    throw(invalid_argument("Wrong date format")
+  }
+  if(time[2] != ':' || (time.substr(6,2) != "AM" || time.substr(6,2) != "PM")) {
+     throw(invalid_argument("Wrong time format")
+  }
+  
+  in testing:
+    EXPECT_THROW( ,invalid_argument)
+*/
+
+
 Event::Event(string name, string date, string time, string details, double duration) {
     this->name = name;
     this->date = date;
