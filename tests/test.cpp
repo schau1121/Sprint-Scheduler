51d44f7022c9f62ff89c4a5c64473c20939603f2
schau1121 <simon.chau1121@gmail.com>
2021-05-27 00:33:44 -0700

    Commented out test again

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,10 @@
 #include "gtest/gtest.h"
-//#include "sortdate_test.cpp"
-//#include "sortpriority_test.cpp"
-//#include "task_test.cpp"
-//#include "tasklist_test.cpp"
+#include "sortdate_test.cpp"
+#include "sortpriority_test.cpp"
+#include "task_test.cpp"
+#include "tasklist_test.cpp"
 #include "event_test.cpp"
-//#include "create_tests.cpp"
+#include "create_tests.cpp"
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
