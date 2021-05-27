27bb8e473d80ac0a6a516d0ff507a2d014dda690
schau1121 <simon.chau1121@gmail.com>
2021-05-27 00:23:35 -0700

    Removed driver code from main

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,6 @@
 using namespace std;
 
 int main() {
-  Event event = Event("Productivity block", "05/28/21", "10:00 AM", "Finish homework", 2.0);
-  TaskList list = TaskList("Homework", "Finish project section", 3);
-  list.addSubTask(Task("CS100 Final Project Sprint", "06/01/21", "Finish edit feature", 5));
-  event.addSubTask(list);
-  event.edit();
   
   return 0;
 }
