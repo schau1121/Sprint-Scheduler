9838bdc9dd8259764f9048625bc853ab376260fb
Simon Chau <simon.chau1121@gmail.com>
2021-05-27 15:37:47 -0700

    Added json library

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 #include <vector>
 #include "client.cpp"
 #include "tasklist.cpp"
 #include "task.cpp"
 #include "event.cpp"
 
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 using namespace std;
 
 int main() {
-  
+  ofstream outFile;
+  outFile.open("save.json");
+  if(outFile.is_open()) {
+    json j;
+    j["task"]["name"] = "Task 1";
+    outFile << setw(4) << j << endl;
+  }
+
+
   return 0;
 }
